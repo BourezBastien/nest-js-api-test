4f66bbd79b9d35caf8f77ffc45374ce376ae2362
BourezBastien <Bourezbastien@gmail.com>
2023-01-31 17:13:05 +0100

    feat(common): initial commit - add project structure for api

---
+++ src/feed/controllers/feed.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Param } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Delete,
+  Put
+} from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { Body } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { FeedPost } from '../models/post.interface';
+import { FeedService } from '../services/feed.service';
+
+@Controller('feed')
+export class FeedController {
+  constructor(private feedService: FeedService) {}
+
+  @Post()
+  create(@Body() feedPost: FeedPost): Observable<FeedPost> {
+    return this.feedService.createPost(feedPost);
+  }
+
+  @Get()
+  findAll(): Observable<FeedPost[]> {
+    return this.feedService.findAllPosts();
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() feedPost: FeedPost,
+  ): Observable<UpdateResult> {
+    return this.feedService.updatePost(id, feedPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.feedService.deletePost(id);
+  }
+}
