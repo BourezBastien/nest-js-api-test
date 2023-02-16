91c308012e8d2f9792f1e0edc81eae4921e5f6d9
Bourez Bastien <Bourezbastien@gmail.com>
2023-02-16 15:56:03 +0100

    feat(app): delete app service and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,9 +6,4 @@ import { AppService } from './app.service';
 @ApiTags('Test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
