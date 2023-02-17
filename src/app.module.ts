6795dc1470266ed8474dbf0450ce203b6583c8d2
Bourez Bastien <Bourezbastien@gmail.com>
2023-02-17 14:48:13 +0100

    feat(common): remove auth module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AccountModule } from './account/account.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -24,7 +23,6 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     AccountModule,
     TransactionsModule,
-    AuthModule,
   ], 
   controllers: [AppController],
   providers: [AppService],
